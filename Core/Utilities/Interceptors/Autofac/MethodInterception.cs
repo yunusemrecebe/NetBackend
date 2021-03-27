0ab98552863a3ee28bc976a9155bc618057caec9
Yunus Emre Cebe <yunusemrecebe@gmail.com>
2021-03-27 22:19:49 +0300

    Interceptors Altyapısı Kurulumu

---
+++ Core/Utilities/Interceptors/Autofac/MethodInterception.cs
@@ -0,0 +1,58 @@
+﻿using Castle.Core.Interceptor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Interceptors.Autofac
+{
+    public abstract class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation)
+        {
+
+        }
+
+        protected virtual void OnAfter(IInvocation invocation)
+        {
+
+        }
+
+        protected virtual void OnException(IInvocation invocation)
+        {
+
+        }
+
+        protected virtual void OnSuccess(IInvocation invocation)
+        {
+
+        }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+            OnBefore(invocation);
+
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception)
+            {
+                isSuccess = false;
+                OnException(invocation);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+            }
+
+            OnAfter(invocation);
+        }
+    }
+}
